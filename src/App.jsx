8c00c01522fae077aa1194d23f788247adc64bf3
MohammadRezaSaeeedi <M.R.Saeedi2024@gmail.com>
2024-05-27 23:55:13 +0330

    add Routes Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import ProductsPage from "./pages/ProductsPage"
+import DetailsPage from "./pages/DetailsPage"
+import CheckoutPage from "./pages/CheckoutPage"
+import PageNotFound from "./pages/404"
 
 function App() {
 
 
   return (
-    <>
-    <h3>RTK-STORE-PROJECT-SPA</h3>
-    </>
+  <Routes>
+  <Route index element={<Navigate to="/products" replace /> } />
+  <Route path="/products" element={<ProductsPage />} />
+  <Route path="/products/:id" element={<DetailsPage />} />
+  <Route path="/products" element={ <CheckoutPage />} />
+  <Route path="/*" element={<PageNotFound />} />
+  </Routes>
   )
 }
 
